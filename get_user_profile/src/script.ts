a213562d690049eda81475bb8d8d40c061d460c2
bondhemlig <stenhammarelias@gmail.com>
2023-06-03 17:45:15 +0200

    switch location

--- get_user_profile/src/script.ts
+++ get_user_profile/src/script.ts
@@ -2,7 +2,7 @@
 // we detect a callback from Spotify by checking for the hash fragment
 import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
 
-const clientId = "your_client_id";
+const clientId = "10bf6bf40e284341b19223b75e562a05";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
